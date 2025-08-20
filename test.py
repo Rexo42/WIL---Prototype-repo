eaeb5b4f9b2a078a12afc405291d65ae4c49db5b
Thomas Marion <tmarion19@outlook.com>
2025-08-20 12:20:04 +1000

    reformat
    created functions for functionality to be more readable and clean

--- test.py
+++ test.py
@@ -2,20 +2,38 @@ import requests
 import json
 
 ACCESS_TOKEN = '8077324ef83f67fbc7b0507e1e03ec85ff6a4655'
-BASE_URL = 'https://enterprise-sandbox-au.simprosuite.com/api/v1.0/companies/'
+BASE_URL = 'https://enterprise-sandbox-au.simprosuite.com/api/v1.0/'
 COMPANY_NAME = "Evergreen Electrical"
+
+#function definitions
+def get_Jobs(ID):
+    URL = BASE_URL+"companies/"+str(ID)+'/jobs/'
+    return requests.get(URL, headers=headers)
+
+def get_Company():
+    URL = BASE_URL+"companies/"
+    companies = requests.get(URL, headers=headers).json()
+    for company in companies:
+        if company.get("Name") == COMPANY_NAME:
+            print("found company:")
+            idNum = company.get("ID")
+            
+            return requests.get(BASE_URL+"companies/"+str(idNum), headers=headers).json()    
+    return "company not found!"
+###
 
 headers = {
     'Authorization': f'Bearer {ACCESS_TOKEN}',
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
-response = requests.get(BASE_URL, headers=headers)
-print(f"response code: {response.status_code}") #200 = success 402 = wrong token 400 = cooked
-companies = response.json()
-for company in companies:
-    if company.get("Name") == COMPANY_NAME:
-        selected = company
-        print("found company:")
-        print(f"Name = {selected.get('Name')}")
-        print(f"ID = {selected.get('ID')}")
+## pulls and prints all details related to the company
+company = get_Company()
+print(json.dumps(company, indent=2))
+
+## gets all jobs from the company and prints
+jobList = get_Jobs(company.get("ID"))
+print(json.dumps(jobList.json(), indent=2))
+
+del company, jobList
+
