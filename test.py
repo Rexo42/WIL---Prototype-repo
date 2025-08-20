1c3a8df404bf4150472cb3121a1e487102f70b02
Thomas Marion <tmarion19@outlook.com>
2025-08-20 11:05:51 +1000

    API endpoint Connection
    test connection to an API endpoint
     - finds our company, ID and Name

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 import requests
+import json
 
 ACCESS_TOKEN = '8077324ef83f67fbc7b0507e1e03ec85ff6a4655'
+BASE_URL = 'https://enterprise-sandbox-au.simprosuite.com/api/v1.0/companies/'
+COMPANY_NAME = "Evergreen Electrical"
+
+headers = {
+    'Authorization': f'Bearer {ACCESS_TOKEN}',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+response = requests.get(BASE_URL, headers=headers)
+print(f"response code: {response.status_code}") #200 = success 402 = wrong token 400 = cooked
+companies = response.json()
+for company in companies:
+    if company.get("Name") == COMPANY_NAME:
+        selected = company
+        print("found company:")
+        print(f"Name = {selected.get('Name')}")
+        print(f"ID = {selected.get('ID')}")
