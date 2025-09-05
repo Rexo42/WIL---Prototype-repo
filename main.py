1ca55cd9da66f82b85d4f37e49aabb311f28b071
Thomas Marion <tmarion19@outlook.com>
2025-09-05 13:50:28 +1000

    removed unecessary comments, cleaned up codebase

--- main.py
+++ main.py
@@ -102,17 +102,14 @@ def test_Requests(ID): #function for running all test API requests and prints ou
     del company, jobList, customers
 
 def get_Job_Logs(ID, JobID):
-    params = {
-    "search": "all",
-    "columns": "Type,Message,Date"
-}
-    relevantNotes = []
     URL = BASE_URL+"companies/"+str(ID)+"/jobs/"+str(JobID)+"/timelines/"
     response = requests.get(URL, headers=headers)
     res = response.json()
     for note in res:
         print(note.get("Type"))
         if note.get("Type") == "Customer Note" or note.get("Type") == "Work Order Technician Notes":
+            if note.get("Type") == "Work Order Technician Notes":
+                print(note)
             return note
 
 class API:
@@ -135,10 +132,16 @@ class API:
                 continue
             result = get_Job_Logs(self.ID, job.get("ID"))
             if (not result):
-                print(f"{currentID} unable to find a valid work note for job... SKIPPING")
+                print(f"skipping job... ({currentID}) REASON: no valid job notes found")
                 continue
             filtered = htmlUtility.strip_html(result.get("Message"))
             editedMessage = evergreenAgent.sendNotes(filtered)
+            # IF CONTAINS REWRITE F THEN ITS INCOMPLETE AND SKIP
+            # ELSE IF CONTAINS JUST REWRITE AND NO F THEN MOVE IT TO COMPLETE
+            # if ("[REWRITE] F" in editedMessage and !()):
+
+            
+
 
             payload = {
             "Description" : editedMessage
